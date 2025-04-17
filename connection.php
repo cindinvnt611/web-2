9c8a932a3cd5dac7100594500ef2dc6b69b263bf
cindinvnt611 <0110124072@student.nurulfikri.ac.id>
2025-04-17 10:59:50 +0700

    percobaan 1

--- connection.php
+++ connection.php
@@ -1,15 +1,20 @@
-<?php 
- 
-$host = 'localhost'; 
-$db = 'learn_pdo'; 
-$user = 'root'; 
-$password = ''; 
- 
-$dsn = "mysql:host=$host;dbname=$db;charset=UTF8"; 
- 
-try { 
-   $pdo = new PDO($dsn, $user, $password); 
-   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-} catch (PDOException $e) {
-    echo $e->getMessage(); 
+<?php
+
+require_once 'config.php';
+
+class connection2
+{
+    public static function make($host, $db, $user, $password)
+    
+{
+    $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+    try {
+        $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+        return new PDO($dsn, $user, $password, $options);
+    } catch (PDOException $e) {
+        die($e->getMessage());
+        }
+    }
 }
+return connection2::make($host, $db, $user, $password);
