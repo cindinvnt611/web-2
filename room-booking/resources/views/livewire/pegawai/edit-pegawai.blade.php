ae03b002c5b70d489a8ef995ed5d6640811fd063
cindinvnt611 <0110124072@student.nurulfikri.ac.id>
2025-06-02 09:07:03 +0700

    prktikum10

---
+++ room-booking/resources/views/livewire/pegawai/edit-pegawai.blade.php
@@ -0,0 +1,40 @@
+<div class="container mx-auto">
+    <h1 class="text-2xl font-bold mb-4">Edit Pegawai</h1>
+
+    <form wire:submit.prevent="save" class="space-y-4">
+        <flux:input
+            type="text"
+            id="nip"
+            wire:model.defer="nip"
+            label="NIP Pegawai"
+            placeholder="Masukkan NIP Pegawai"
+            required />
+
+        <flux:input
+            type="text"
+            id="nama"
+            wire:model.defer="nama"
+            label="Nama Pegawai"
+            placeholder="Masukkan Nama Pegawai"
+            required />
+
+        <flux:select
+            id="unit_kerja"
+            wire:model.defer="unit_kerja"
+            label="Unit Kerja"
+            placeholder="Pilih Unit Kerja"
+            required>
+
+            <flux:select.option value="">Pilih Unit Kerja</flux:select.option>
+            @foreach ($unit_kerjas as $unit)
+            <flux:select.option value="{{ $unit->id }}">{{ $unit->nama }}</flux:select.option>
+            @endforeach
+        </flux:select>
+
+        <flux:button
+            type="submit"
+            variant="primary">
+            Save
+        </flux:button>
+    </form>
+</div>
---
+++ room-booking/routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+
+use App\Livewire\Counter;
+
+//ruang
+use App\Livewire\Ruang\ListRuang;
+use App\Livewire\Ruang\CreateRuang;
+use App\Livewire\Ruang\EditRuang;
+
+//pegawai
+use App\Livewire\Pegawai\ListPegawai;
+use App\Livewire\Pegawai\CreatePegawai;
+use App\Livewire\Pegawai\EditPegawai;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    
+});
+
+require __DIR__ . '/auth.php';
+
+Route::get('/counter', Counter::class);
+
+//ruang
+Route::get('/ruang', ListRuang::class)->name('ruang.index');
+Route::get('/ruang/create', CreateRuang::class)->name('ruang.create');
+Route::get('/ruang/edit/{ruang}', EditRuang::class)->name('ruang.edit');
+
+//pegawai
+Route::get('/pegawai', ListPegawai::class)->name('pegawai.index');
+Route::get('/pegawai/create', CreatePegawai::class)->name('pegawai.create');
+Route::get('/pegawai/edit/{ruang}', EditPegawai::class)->name('pegawai.edit');
